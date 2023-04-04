be2e7382bd216a006e2afa3c2af66f33c31a4fe7
Testers7777 <97400362+Testers7777@users.noreply.github.com>
2023-04-04 13:40:18 +0200

    Add files via upload

---
+++ src/script.js
@@ -0,0 +1,17 @@
+var text1 = 'Hello'
+var text2 = 'Hallo'
+var text3 = 'Bonjour'
+var speed = '60'
+
+setInterval(() => {
+    const a = document.getElementById('test');
+    if(a.className == text1) {
+        a.innerHTML = a.className
+        a.setAttribute('class', text2)
+    } else if(a.className == text2) {
+        a.innerHTML = a.className
+        a.setAttribute('class', text3)
+    } else if(a.className == text3) {
+        a.innerHTML = a.className
+        a.setAttribute('class', text1)
+    }}, 1000*speed)
