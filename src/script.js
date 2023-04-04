389b9bb8196c00b675f407b5a10c70b8d1f0e06d
Testers7777 <97400362+Testers7777@users.noreply.github.com>
2023-04-04 13:41:16 +0200

    Update script.js

--- src/script.js
+++ src/script.js
@@ -1,7 +1,7 @@
 var text1 = 'Hello'
 var text2 = 'Hallo'
 var text3 = 'Bonjour'
-var speed = '60'
+var speed = '10' // avec cette valeur les textes ce changent toutes les 10 secondes
 
 setInterval(() => {
     const a = document.getElementById('test');
