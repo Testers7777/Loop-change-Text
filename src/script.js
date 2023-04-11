e85c1a34a928ecf839e4d7169645c0dc81b8cf1f
Testers7777 <97400362+Testers7777@users.noreply.github.com>
2023-04-11 14:15:26 +0200

    Update script.js

--- src/script.js
+++ src/script.js
@@ -1,14 +1,11 @@
-const elements = {'text1': 'Hello', 'text2': 'Hallo', 'text3': 'Bonjour', 'speed': 10}
+const elements = {'text1': 'Hello', 'text2': 'Hallo', 'text3': 'Bonjour', 'speed': 2}
 
 setInterval(() => {
     const a = document.getElementById('test');
-    if(a.className == elements['text1']) {
-        a.innerHTML = a.className
-        a.setAttribute('class', elements['text2'])
-    } else if(a.className == elements['text2']) {
-        a.innerHTML = a.className
-        a.setAttribute('class', elements['text3'])
-    } else if(a.className == elements['text3']) {
-        a.innerHTML = a.className
-        a.setAttribute('class', elements['text1'])
-    }}, 1000*elements['speed'])
+    if(a.textContent == elements.text1) {
+        a.innerHTML = elements.text2
+    } else if(a.textContent == elements.text2) {
+        a.innerHTML = elements.text3
+    } else if(a.textContent == elements.text3) {
+        a.innerHTML = elements.text1
+    }}, 1000*elements.speed)
